11fd1f626184647934abb5659445207fdd8e4041
Quentin Vignier <qvignier@igraal.com>
2021-01-03 17:24:29 +0100

    Add properties to view model for workout reference

--- src/View/Model/WorkoutReference/ReferenceWorkout/Nested/ReferenceExerciseInManyWorkoutNestedModel.php
+++ src/View/Model/WorkoutReference/ReferenceWorkout/Nested/ReferenceExerciseInManyWorkoutNestedModel.php
@@ -7,5 +7,9 @@ final class ReferenceExerciseInManyWorkoutNestedModel
     public string $name;
     public string $canonicalName;
     public string $image;
+    public ?int $restDuration; // in seconds
+    public ?int $nbReps;
+    public ?int $distance; // in meters
+    public ?int $duration; // in seconds
     public int $position;
 }
--- src/View/Model/WorkoutReference/ReferenceWorkout/Nested/ReferenceExerciseInOneWorkoutNestedModel.php
+++ src/View/Model/WorkoutReference/ReferenceWorkout/Nested/ReferenceExerciseInOneWorkoutNestedModel.php
@@ -7,6 +7,10 @@ final class ReferenceExerciseInOneWorkoutNestedModel
     public string $name;
     public string $canonicalName;
     public string $image;
+    public ?int $restDuration; // in seconds
+    public ?int $nbReps;
+    public ?int $distance; // in meters
+    public ?int $duration; // in seconds
     public int $position;
 
     /** @var ReferenceExerciseEquipmentNestedModel[] */
